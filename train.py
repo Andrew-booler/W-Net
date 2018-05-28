e0d4b33037ceec136e804a8d3e881524bcf7a2ac
Andrew-booler <andrew.zhangw@gmail.com>
2018-05-27 23:27:17 -0400

    delete the redundant files

--- train.py
+++
@@ -1,9 +0,0 @@
-import torch
-import configure
-import numpy as np
-
-if __name__ == '__main__':
-    dataloader = DataLoader(datapath,"train")
-    for epoch in range(Maxepoch):
-        for step,(x) in enumerate(dataloader):
-
